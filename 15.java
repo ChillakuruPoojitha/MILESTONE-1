ec1a27c09397b05fb02c6aa63cad307b439f3515
ChillakuruPoojitha <82634011+ChillakuruPoojitha@users.noreply.github.com>
2021-08-20 19:03:51 +0530

    Add files via upload

---
+++ 15.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class difference {
+
+public static void main(String[] args) {
+// TODO Auto-generated method stub
+Scanner sc=new Scanner(System.in);
+int n;
+int d=36;
+n=d/9;
+System.out.println(n);
+}
+
+}
